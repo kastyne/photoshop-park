3b3c94a6f774dec2ea142afd47e8bd15d1eebb1a
Kaitlyn <rainesconjecture@gmail.com>
2022-03-10 11:23:09 -0500

    Fix issue of max_length being a string

--- database/models/course.py
+++ database/models/course.py
@@ -2,14 +2,17 @@ from django.db import models
 
 
 class Course(models.Model):
-    name = models.CharField(max_length="50")
-    authors = models.CharField(max_length="50")
-    description = models.CharField(max_length="120")
+    name = models.CharField(max_length=50)
+    authors = models.CharField(max_length=50)
+    description = models.CharField(max_length=120)
     image = models.URLField()
+
+    def __str__(self):
+        return self.name
 
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    name = models.CharField(max_length="50")
-    authors = models.CharField(max_length="50")
-    description = models.CharField(max_length="120")
+    name = models.CharField(max_length=50)
+    authors = models.CharField(max_length=50)
+    description = models.CharField(max_length=120)
