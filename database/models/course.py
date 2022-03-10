3c6937400b63e97e5fd34152e8dce28c4aa9cab7
Kaitlyn <rainesconjecture@gmail.com>
2022-03-10 10:05:13 -0500

    Add authors fields

--- database/models/course.py
+++ database/models/course.py
@@ -1,12 +1,15 @@
 from django.db import models
 
+
 class Course(models.Model):
     name = models.CharField(max_length="50")
+    authors = models.CharField(max_length="50")
     description = models.CharField(max_length="120")
     image = models.URLField()
 
+
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
-
     name = models.CharField(max_length="50")
+    authors = models.CharField(max_length="50")
     description = models.CharField(max_length="120")
