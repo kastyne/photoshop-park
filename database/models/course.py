db13e1929e255ccab97bdc723d0ce0f541cf0e4a
Kaitlyn <rainesconjecture@gmail.com>
2022-03-11 12:28:48 -0500

    Add default attribute

--- database/models/course.py
+++ database/models/course.py
@@ -2,11 +2,12 @@ from django.template.defaultfilters import slugify
 from django.contrib.auth.models import User
 from django.db import models
 
+
 class Course(models.Model):
     title = models.CharField(max_length=120)
     authors = models.CharField(max_length=120)
     description = models.CharField(max_length=240)
-    slug = models.CharField(max_length=120)
+    slug = models.CharField(max_length=120, default="")
     image = models.URLField()
 
     def __str__(self):
@@ -14,22 +15,21 @@ class Course(models.Model):
 
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super(Course, self).save(*args, **kwargs) 
+        super(Course, self).save(*args, **kwargs)
 
 
 class Lesson(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE, related_name="lessons")
-    title = models.CharField(max_length=120)
+    title = models.CharField(max_length=120, default="")
     authors = models.CharField(max_length=50)
     description = models.CharField(max_length=240)
     created_on = models.DateField(auto_now_add=True)
-    slug = models.CharField(max_length=120)
-    content = models.TextField()
+    slug = models.CharField(max_length=120, default="")
+    content = models.TextField(default="")
 
     def __str__(self):
         return self.title
-    
-    
+
     def save(self, *args, **kwargs):
         self.slug = slugify(self.title)
-        super(Lesson, self).save(*args, **kwargs) 
+        super(Lesson, self).save(*args, **kwargs)
