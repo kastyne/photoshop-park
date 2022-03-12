b637942ce19127691a80c69657d83645d17284ac
Kaitlyn <rainesconjecture@gmail.com>
2022-03-12 13:47:57 -0500

    Add a ModelAdmin for blog, switch to decorators

--- database/admin.py
+++ database/admin.py
@@ -1,13 +1,18 @@
 from django.contrib import admin
 from database.models import course, blog
 
+
+@admin.register(course.Course, course.Lesson)
 class CourseAdmin(admin.ModelAdmin):
     list_display = ('title', 'authors')
-    list_filter = ('authors', )
+    list_filter = ('authors',)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
 
-admin.site.register(course.Course, CourseAdmin)
-admin.site.register(course.Lesson, CourseAdmin)
-admin.site.register(blog.Article)
+@admin.register(blog.Article)
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'authors')
+    list_filter = ('authors', 'status')
+    search_fields = ['title', 'content']
+    prepopulated_fields = {'slug': ('title',), 'authors': ('authors',)}
