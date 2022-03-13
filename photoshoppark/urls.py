de0676f7625e8e172bcf7b2089eda54c510a7fd3
Kaitlyn <rainesconjecture@gmail.com>
2022-03-13 19:42:13 -0400

    Update urls.py

--- photoshoppark/urls.py
+++ photoshoppark/urls.py
@@ -23,8 +23,9 @@ urlpatterns = [
     path('', include('home.urls')),
     path('courses/', include('courses.urls')),
     path('blog/', include('blog.urls')),
-    path('register/', register),
+    path('register/', register, name='register'),
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     # path('login/', login),
 
     path('admin/', admin.site.urls),
