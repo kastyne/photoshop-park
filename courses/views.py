afff3b7e84de7b3bc9c50a98c608373241d4ec06
Kaitlyn <rainesconjecture@gmail.com>
2022-03-12 21:50:27 -0500

    Remove useless imports, enforce PEP8 (child needs a linter)

--- courses/views.py
+++ courses/views.py
@@ -1,10 +1,11 @@
-from multiprocessing import get_context
 from database.models.course import Course, Lesson
 from django.views import generic
+
 
 class CourseList(generic.ListView):
     model = Course
     template_name = 'courses/index.html'
+
 
 class CourseDetails(generic.DetailView):
     model = Course
@@ -13,8 +14,9 @@ class CourseDetails(generic.DetailView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['lessons'] = context['object'].lessons.all()
-        
+
         return context
+
 
 class LessonDetails(generic.base.TemplateView):
     template_name = 'courses/lesson_details.html'
