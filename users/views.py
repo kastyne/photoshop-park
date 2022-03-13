86d6c1b3316c26aa7f2440dbba63a5072724a6a9
Kaitlyn <rainesconjecture@gmail.com>
2022-03-13 18:14:13 -0400

    Subclass UserCreationForm and add email field

--- users/views.py
+++ users/views.py
@@ -1,15 +1,18 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
+from .forms import UserRegisterationForm
 
 
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = UserRegisterationForm(request.POST)
 
         if form.is_valid():
+            form.save()
             username = form.cleaned_data.get('username')
             messages.success(request, f"Welcome to Photoshop Park, {username}!")
+            return redirect('homepage')
     else:
-        form = UserCreationForm()
+        form = UserRegisterationForm()
+
     return render(request, 'users/register.html', {'form': form})
