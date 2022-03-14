2676f3abb4648474a7cb750be0512d0253f7d8a8
Haaruun Ibrow <haaruunibrow@gmail.com>
2022-03-14 09:02:35 +0300

    som login fixes

--- photoshoppark/urls.py
+++ photoshoppark/urls.py
@@ -16,17 +16,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-from users.views import register, login
-from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     path('', include('home.urls')),
+    path('', include('users.urls')),
     path('courses/', include('courses.urls')),
     path('blog/', include('blog.urls')),
-    path('register/', register, name='register'),
-    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
-    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    # path('login/', login),
-
     path('admin/', admin.site.urls),
 ]
--- users/urls.py
+++ users/urls.py
@@ -1,3 +1,9 @@
-from django.urls import path, include
+from django.urls import path
+from django.contrib.auth import views as auth_view
 from users.views import register
 
+urlpatterns = [
+    path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('register/', register)
+]
