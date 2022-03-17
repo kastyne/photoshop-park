a2dfd71b8f9b8d8656bc35b463cc7bc73dfea027
Kaitlyn <rainesconjecture@gmail.com>
2022-03-17 15:44:23 -0400

    Add PsUser admin

--- database/admin.py
+++ database/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from database.models import course, blog
+from database.models import course, blog, user
 
 
 @admin.register(course.Course, course.Lesson)
@@ -9,6 +9,7 @@ class CourseAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
+
 @admin.register(blog.Article)
 class BlogAdmin(admin.ModelAdmin):
     list_display = ('title', 'authors')
@@ -16,6 +17,12 @@ class BlogAdmin(admin.ModelAdmin):
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
 
+
 @admin.register(blog.Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name', )
+    list_display = ('name',)
+
+
+@admin.register(user.PsUser)
+class PsUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'first_name', 'last_name', 'is_staff', 'date_joined')
