c476e758bcbf9ee41c94ec0a8ad83dd553380c4e
Kaitlyn <rainesconjecture@gmail.com>
2022-03-17 15:45:12 -0400

    Add a simple profile path

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.contrib.auth import views as auth_view
-from users.views import register
+from users.views import register, profile
 
 urlpatterns = [
     path('login/', auth_view.LoginView.as_view(template_name='users/login.html'), name='login'),
     path('logout/', auth_view.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
-    path('register/', register)
+    path('register/', register, name='register'),
+    path('profile/', profile, name='profile')
 ]
