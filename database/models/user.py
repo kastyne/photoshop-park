cb8664d3577a6232f5376f192d6a1e41848f172e
Kaitlyn <rainesconjecture@gmail.com>
2022-03-17 15:43:06 -0400

    Add a custom PsUser model

---
+++ database/models/user.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+
+class PsUser(AbstractUser):
+    username = models.CharField(max_length=64, default='', unique=True)
+    email = models.EmailField(max_length=64, unique=True)
+    USERNAME_FIELD = 'username'
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return self.username
