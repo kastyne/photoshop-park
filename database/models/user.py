80a0f32cbaafada1406e6a952a93c89f7b6460ee
Kaitlyn <rainesconjecture@gmail.com>
2022-05-19 22:57:01 -0400

    Add avatar to PsUser

--- database/models/user.py
+++ database/models/user.py
@@ -4,6 +4,7 @@ from django.db import models
 
 class PsUser(AbstractUser):
     username = models.CharField(max_length=64, default='', unique=True)
+    avatar = models.URLField(blank=True, default='https://i.scdn.co/image/ab6761610000e5ebfd30ebd7e80dad6b2383aab0')
     email = models.EmailField(max_length=64, unique=True)
     enrollment = models.ManyToManyField('Course', through='Enrollment')
 
