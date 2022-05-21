ebdb6306b0e5522ace7869ace7f4c4f400a096ec
Haaruun <haaruunibrow@gmail.com>
2022-05-21 20:07:22 +0300

    popup images + attempted styling

--- static/main.js
+++ static/main.js
@@ -12,3 +12,34 @@ window.onscroll = () => {
 
     prevScrollPos = currScrollPos
 }
+
+let images = document.querySelectorAll('img').forEach(img => {
+    img.addEventListener('click', _ => {
+        let figure = document.createElement('figure')
+        let figureImage = document.createElement('img')
+        let figureCaption = document.createElement('figcaption')
+
+        figureImage.src = img.src
+        figureCaption.innerText = img.alt // if it does not have an alt, thats your problem not mine
+        
+        figure.appendChild(figureImage)
+        figure.appendChild(figureCaption)
+
+        let wrapper = document.createElement('div')
+        let underlay = document.createElement('div')
+
+        underlay.classList.add('underlay')
+        wrapper.classList.add('fullscreenImage')
+
+        wrapper.appendChild(underlay)
+        wrapper.appendChild(figure)
+
+        underlay.addEventListener('click', _ => {
+            console.log('g')
+            wrapper.remove()
+        }, {once: true})
+
+        document.body.appendChild(wrapper)
+
+    })
+})
