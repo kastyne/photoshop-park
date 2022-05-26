cc6f7b050a15309f31a648307614c654327ca1d8
Kaitlyn <rainesconjecture@gmail.com>
2022-05-26 12:33:29 -0400

    Revert "Merge pull request #2 from MrBalint91/development"
    This reverts commit 5d46fd9c74333673d37303e61bc87e69a409b24c, reversing
    changes made to 137819c2f9e354cf6d13dcb5548be9f7f4d883f0.

--- furry-potato/artwork/urls.py
+++
@@ -1,8 +0,0 @@
-import imp
-from django.urls import path
-from .views import ArtworkList, ArtworkDetails
-
-urlpatterns = [
-    path('', ArtworkList.as_view(), name="artwork_list"),
-    path('<slug:username>/<slug:artwork>', ArtworkDetails.as_view(), name="artwork_details")
-]
--- furry-potato/blog/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path, include
-from blog import views
-
-urlpatterns = [
-    path('', views.ArticleList.as_view()),
-    path('drafts/', views.DraftList.as_view(), name='draft_list'),
-    path('<slug:slug>/', views.ArticleDetails.as_view(), name='article_detail')
-]
