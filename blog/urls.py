6eee866bb34f0ad148d98fd93722e02222530050
Haaruun <haaruunibrow@gmail.com>
2022-09-10 14:09:19 +0300

    remove the category list

--- blog/urls.py
+++ blog/urls.py
@@ -4,7 +4,6 @@ from blog import views
 urlpatterns = [
     path('', views.ArticleList.as_view()),
     path('drafts/', views.DraftList.as_view(), name='draft_list'),
-    path('categories/', views.CategoryList.as_view(), name="category_list"),
     path('categories/<slug:title>/', views.CategoryDetails.as_view(), name='category_details'),
     path('<slug:slug>/', views.ArticleDetails.as_view(), name='article_detail'),
 ]
